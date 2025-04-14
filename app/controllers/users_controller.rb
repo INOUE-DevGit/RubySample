4fcca6f5723a7404510575a3eaf6b4d4a35815b9
井上 <173559396+INOUE-DevGit@users.noreply.github.com>
2025-04-15 01:08:33 +0900

    インスタンス変数の追加
    3-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
+    # インスタンス変数はビューで使用できる
+    @hello = "Hello, World!"
+
     # ビューがviews/コントローラー名/ビュー名.html.erbならrenderは省略できる
     render template: "users/index"
   end
