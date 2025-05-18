748442b1b34adcdf851f31ea2a8d178e72edc7f0
井上 <173559396+INOUE-DevGit@users.noreply.github.com>
2025-05-19 01:22:09 +0900

    スキーマファイルを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_11_155008) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_18_152231) do
   create_table "diaries", force: :cascade do |t|
     t.string "title"
     t.text "body"
