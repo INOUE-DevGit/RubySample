28d79b6f21fad20b68f70a1090ba0656eb27928b
井上 <173559396+INOUE-DevGit@users.noreply.github.com>
2025-05-19 01:31:06 +0900

    スキーマファイルを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :diaries
+  # localhost:3000で一覧画面に遷移させる(#indexまで書かないと遷移しない)
+  root "diaries#index"
+
   # For details on the DSL available within this file, see http:guides.rubyonrails.org/routing.html
   get "/users", to: "users#index"
 
